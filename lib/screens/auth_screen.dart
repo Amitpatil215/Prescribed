271470d940e4321c4c12e8316954fa98927f55ee
Amit Patil <amitgpatil215@gmail.com>
2020-08-29 15:00:30 +0530

    Adding drop down button in gender form field

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -23,7 +23,7 @@ class AuthScreen extends StatelessWidget {
                 ),
                 ASAuthForm(),
               ],
-            )),
+            ),),
       ),
     );
   }
--- lib/screens/profile/profile_edit_screen.dart
+++ lib/screens/profile/profile_edit_screen.dart
@@ -1,9 +1,37 @@
 import 'package:flutter/material.dart';
 
-class ProfileEditScreen extends StatelessWidget {
+class ProfileEditScreen extends StatefulWidget {
   static const routName = '/profile-edit';
+
+  @override
+  _ProfileEditScreenState createState() => _ProfileEditScreenState();
+}
+
+class _ProfileEditScreenState extends State<ProfileEditScreen> {
+  final _formKey = GlobalKey<FormState>();
+  int _selectedGender = 0;
+
+  List<DropdownMenuItem<int>> genderList = [];
+
+  void loadGenderList() {
+    genderList = [];
+    genderList.add(new DropdownMenuItem(
+      child: new Text('Male'),
+      value: 0,
+    ));
+    genderList.add(new DropdownMenuItem(
+      child: new Text('Female'),
+      value: 1,
+    ));
+    genderList.add(new DropdownMenuItem(
+      child: new Text('Other'),
+      value: 2,
+    ));
+  }
+
   @override
   Widget build(BuildContext context) {
+    loadGenderList();
     return Scaffold(
         backgroundColor: Theme.of(context).backgroundColor,
         appBar: AppBar(
@@ -11,83 +39,154 @@ class ProfileEditScreen extends StatelessWidget {
         ),
         body: Container(
           margin: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-          child: Column(
-            children: [
-              EachFormTile(
-                hintText: "e.g. Zakir Khan",
-                iconRecieved: Icons.person,
-                labelText: "Your Full Name",
-              ),
-              EachFormTile(
-                hintText: "e.g. Zakir Khan",
-                iconRecieved: null,
-                labelText: "Gender",
-              ),
-              EachFormTile(
-                hintText: "e.g. Noida",
-                iconRecieved: Icons.location_on,
-                labelText: "Home Location",
-              ),
-              EachFormTile(
-                hintText: "e.g. +91 1234567890",
-                iconRecieved: Icons.call,
-                labelText: "Contact Number",
-              ),
-              EachFormTile(
-                hintText: "e.g. doctorDuniya@gmail.com",
-                iconRecieved: Icons.email,
-                labelText: "Email",
-              ),
-            ],
-          ),
-        ));
-  }
-}
-
-class EachFormTile extends StatelessWidget {
-  final String labelText;
-  final IconData iconRecieved;
-  final String hintText;
-
-  EachFormTile({
-    Key key,
-    this.labelText,
-    this.iconRecieved,
-    this.hintText,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Form(
-      child: Column(
-        children: [
-          Container(
-            margin: EdgeInsets.symmetric(vertical: 10),
-            child: Row(
+          child: Form(
+            child: Column(
               children: [
-                Icon(
-                  iconRecieved,
-                  size: 30,
-                ),
-                SizedBox(
-                  width: 15,
-                ),
-                Expanded(
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                      hintText: hintText,
-                      labelText: labelText,
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(5),
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 10),
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.person,
+                        size: 30,
                       ),
-                    ),
+                      SizedBox(
+                        width: 15,
+                      ),
+                      Expanded(
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            hintText: "e.g Zakir Khan",
+                            labelText: "Your Full Name",
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(5),
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
                   ),
-                )
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 10),
+                  child: Row(
+                    children: [
+                      Icon(
+                        null,
+                        size: 30,
+                      ),
+                      SizedBox(
+                        width: 15,
+                      ),
+                      Expanded(
+                        child: FormField(
+                          builder: (FormFieldState<String> state) {
+                            return InputDecorator(
+                              decoration: InputDecoration(
+                                labelText: "Gender",
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(5),
+                                ),
+                              ),
+                              child: DropdownButtonHideUnderline(
+                                child: DropdownButton(
+                                  isDense: true,
+                                  hint: Text("Select Gender"),
+                                  items: [...genderList],
+                                  value: _selectedGender,
+                                  isExpanded: true,
+                                  onChanged: (value) {
+                                    setState(() {
+                                      _selectedGender = value;
+                                    });
+                                  },
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 10),
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.location_on,
+                        size: 30,
+                      ),
+                      SizedBox(
+                        width: 15,
+                      ),
+                      Expanded(
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            hintText: "e.g. Noida",
+                            labelText: "Home Location",
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(5),
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 10),
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.phone,
+                        size: 30,
+                      ),
+                      SizedBox(
+                        width: 15,
+                      ),
+                      Expanded(
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            hintText: "e.g. +91 1234567890",
+                            labelText: "Contact Number",
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(5),
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 10),
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.email,
+                        size: 30,
+                      ),
+                      SizedBox(
+                        width: 15,
+                      ),
+                      Expanded(
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            hintText: "e.g. doctorDuniya@gmail.com",
+                            labelText: "Email Address",
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(5),
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
               ],
             ),
-          )
-        ],
-      ),
-    );
+          ),
+        ));
   }
 }
