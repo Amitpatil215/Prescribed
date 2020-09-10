6ce2f57e8876b0449738ea3c1506440bcc5aad89
Amit Patil <amitgpatil215@gmail.com>
2020-09-10 19:22:28 +0530

    Making app bar eexpandable

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -22,150 +22,191 @@ class HomeScreen extends StatelessWidget {
               size: 30,
             ),
             onPressed: () {}),
-        body: ListView(
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Container(
-                  width: double.infinity,
-                  margin: EdgeInsets.symmetric(vertical: 20),
-                  child: Row(
+        body: NestedScrollView(
+          headerSliverBuilder: (context, innerBoxIsScrolled) {
+            return <Widget>[
+              SliverAppBar(
+                backgroundColor: Colors.transparent,
+                expandedHeight: 0.2.hp,
+                pinned: true,
+                elevation: 0,
+                floating: true,
+                flexibleSpace: FlexibleSpaceBar(
+                  titlePadding: EdgeInsets.symmetric(
+                    horizontal: 10,
+                    vertical: 10,
+                  ),
+                  title: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Container(
-                        margin: EdgeInsets.symmetric(horizontal: 8),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            Text("Clinic near"),
-                            FlatButton.icon(
-                              onPressed: () {},
-                              icon: Icon(Icons.location_on),
-                              label: Text("Pune"),
-                            )
-                          ],
+                      Text(
+                        "Doctor Duniya",
+                        style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.w300,
+                          color: Colors.black,
                         ),
                       ),
-                      Padding(
-                        padding: const EdgeInsets.all(20.0),
-                        child: GestureDetector(
-                          child: CircleAvatar(),
+                      GestureDetector(
+                        child: CircleAvatar(
+                          maxRadius: 12.h,
+                        ),
+                        onTap: () {
+                          Navigator.of(context)
+                              .pushNamed(ProfileScreen.rountName);
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ];
+          },
+          body: ListView(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  // Container(
+                  //   width: double.infinity,
+                  //   margin: EdgeInsets.symmetric(vertical: 20),
+                  //   child: Row(
+                  //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  //     children: [
+                  //       Container(
+                  //         margin: EdgeInsets.symmetric(horizontal: 8),
+                  //         child: Row(
+                  //           mainAxisAlignment: MainAxisAlignment.start,
+                  //           children: [
+                  //             Text("Clinic near"),
+                  //             FlatButton.icon(
+                  //               onPressed: () {},
+                  //               icon: Icon(Icons.location_on),
+                  //               label: Text("Pune"),
+                  //             )
+                  //           ],
+                  //         ),
+                  //       ),
+                  //       Padding(
+                  //         padding: const EdgeInsets.all(20.0),
+                  //         child: GestureDetector(
+                  //           child: CircleAvatar(),
+                  //           onTap: () {
+                  //             Navigator.of(context)
+                  //                 .pushNamed(ProfileScreen.rountName);
+                  //           },
+                  //         ),
+                  //       ),
+                  //     ],
+                  //   ),
+                  // ),
+                  // Padding(
+                  //   padding: EdgeInsets.only(left: 8.0),
+                  //   child: Text(
+                  //     "Appointment with \n a doctor",
+                  //     textAlign: TextAlign.left,
+                  //     style: TextStyle(
+                  //       fontSize: 30,
+                  //       fontWeight: FontWeight.bold,
+                  //     ),
+                  //   ),
+                  // ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(
+                      left: 10.0,
+                      right: 20.0,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Categories",
+                          style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        InkWell(
+                          child: Text(
+                            "See All",
+                            style: TextStyle(
+                              fontSize: 14,
+                              color: Theme.of(context).accentColor,
+                            ),
+                          ),
                           onTap: () {
                             Navigator.of(context)
-                                .pushNamed(ProfileScreen.rountName);
+                                .pushNamed(CatergoriesSeeAllScreen.routName);
                           },
                         ),
-                      ),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(left: 8.0),
-                  child: Text(
-                    "Appointment with \n a doctor",
-                    textAlign: TextAlign.left,
-                    style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
+                      ],
                     ),
                   ),
-                ),
-                SizedBox(
-                  height: 8,
-                ),
-                Padding(
-                  padding: EdgeInsets.only(
-                    left: 10.0,
-                    right: 20.0,
+                  Container(
+                    margin: EdgeInsets.symmetric(
+                      vertical: 20,
+                      horizontal: 10,
+                    ),
+                    height: 120,
+                    width: double.infinity,
+                    child: HSGridView(),
                   ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        "Categories",
-                        style: TextStyle(
-                          fontSize: 15,
-                          fontWeight: FontWeight.bold,
-                        ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(left: 10.0),
+                    child: Text(
+                      "How can we help you?",
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
                       ),
-                      InkWell(
-                        child: Text(
-                          "See All",
-                          style: TextStyle(
-                            fontSize: 14,
-                            color: Theme.of(context).accentColor,
-                          ),
-                        ),
-                        onTap: () {
-                          Navigator.of(context)
-                              .pushNamed(CatergoriesSeeAllScreen.routName);
-                        },
-                      ),
-                    ],
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.symmetric(
-                    vertical: 20,
-                    horizontal: 10,
-                  ),
-                  height: 120,
-                  width: double.infinity,
-                  child: HSGridView(),
-                ),
-                SizedBox(
-                  height: 8,
-                ),
-                Padding(
-                  padding: EdgeInsets.only(left: 10.0),
-                  child: Text(
-                    "How can we help you?",
-                    style: TextStyle(
-                      fontSize: 15,
-                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                ),
-                HSHelpButton(),
-                SizedBox(
-                  height: 8,
-                ),
-                Padding(
-                  padding: EdgeInsets.only(left: 10.0, right: 20.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        "Top Doctors",
-                        style: TextStyle(
-                          fontSize: 15,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      InkWell(
-                        child: Text(
-                          "See All",
+                  HSHelpButton(),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(left: 10.0, right: 20.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Top Doctors",
                           style: TextStyle(
-                            fontSize: 14,
-                            color: Theme.of(context).accentColor,
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
                           ),
                         ),
-                        onTap: () {
-                          Navigator.of(context)
-                              .pushNamed(TopDoctorsSeeAllScreen.routeName);
-                        },
-                      ),
-                    ],
+                        InkWell(
+                          child: Text(
+                            "See All",
+                            style: TextStyle(
+                              fontSize: 14,
+                              color: Theme.of(context).accentColor,
+                            ),
+                          ),
+                          onTap: () {
+                            Navigator.of(context)
+                                .pushNamed(TopDoctorsSeeAllScreen.routeName);
+                          },
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                HSListView(),
-              ],
-            ),
-          ],
+                  SizedBox(
+                    height: 10,
+                  ),
+                  HSListView(),
+                ],
+              ),
+            ],
+          ),
         ));
   }
 }
