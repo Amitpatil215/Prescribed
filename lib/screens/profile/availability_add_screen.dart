8dc0ea26b22d9769a219891480ff640b26f1c41f
Amit Patil <amitgpatil215@gmail.com>
2020-09-18 16:18:18 +0530

    Availability tile in profile screen & add availability screen added

---
+++ lib/screens/profile/availability_add_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AvailabilityAddScreen extends StatelessWidget {
+  static const routName = "";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Availability"),
+      ),
+    );
+  }
+}
