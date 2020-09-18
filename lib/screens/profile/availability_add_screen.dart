3576718348fe22f60d1c802103a77b8be0f38975
Amit Patil <amitgpatil215@gmail.com>
2020-09-18 17:56:19 +0530

    Update Availability UI improvements

--- lib/screens/profile/availability_add_screen.dart
+++ lib/screens/profile/availability_add_screen.dart
@@ -1,13 +1,177 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
+import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
+import 'package:intl/intl.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class AvailabilityAddScreen extends StatelessWidget {
   static const routName = "";
+  final format = DateFormat("yyyy-MM-dd hh:mm a");
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Availability"),
+        title: Text("Update Availability"),
       ),
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 7, vertical: 15),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Title("Telemedication Consultaion", FlutterIcons.call_mdi),
+              SizedBox(height: 10),
+              Container(
+                width: 0.95.wp,
+                child: DateTimeField(
+                  format: format,
+                  decoration: InputDecoration(
+                    labelText: "from",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                  ),
+                  onShowPicker: (context, currentValue) async {
+                    final date = await showDatePicker(
+                      context: context,
+                      firstDate: DateTime.now(),
+                      initialDate: currentValue ?? DateTime.now(),
+                      lastDate: DateTime(2025),
+                    );
+                    if (date != null) {
+                      final time = await showTimePicker(
+                        context: context,
+                        initialTime: TimeOfDay.fromDateTime(
+                            currentValue ?? DateTime.now()),
+                      );
+                      return DateTimeField.combine(date, time);
+                    } else {
+                      return currentValue;
+                    }
+                  },
+                ),
+              ),
+              SizedBox(height: 10),
+              Container(
+                width: 0.95.wp,
+                child: DateTimeField(
+                  format: format,
+                  decoration: InputDecoration(
+                    labelText: "To",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                  ),
+                  onShowPicker: (context, currentValue) async {
+                    final date = await showDatePicker(
+                      context: context,
+                      firstDate: DateTime.now(),
+                      initialDate: currentValue ?? DateTime.now(),
+                      lastDate: DateTime(2025),
+                    );
+                    if (date != null) {
+                      final time = await showTimePicker(
+                        context: context,
+                        initialTime: TimeOfDay.fromDateTime(
+                            currentValue ?? DateTime.now()),
+                      );
+                      return DateTimeField.combine(date, time);
+                    } else {
+                      return currentValue;
+                    }
+                  },
+                ),
+              ),
+              SizedBox(height: 20),
+              Title("Clinic Consultaion", FlutterIcons.hospital_alt_faw5s),
+              SizedBox(height: 10),
+              Container(
+                width: 0.95.wp,
+                child: DateTimeField(
+                  format: format,
+                  decoration: InputDecoration(
+                    labelText: "from",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                  ),
+                  onShowPicker: (context, currentValue) async {
+                    final date = await showDatePicker(
+                      context: context,
+                      firstDate: DateTime.now(),
+                      initialDate: currentValue ?? DateTime.now(),
+                      lastDate: DateTime(2025),
+                    );
+                    if (date != null) {
+                      final time = await showTimePicker(
+                        context: context,
+                        initialTime: TimeOfDay.fromDateTime(
+                            currentValue ?? DateTime.now()),
+                      );
+                      return DateTimeField.combine(date, time);
+                    } else {
+                      return currentValue;
+                    }
+                  },
+                ),
+              ),
+              SizedBox(height: 10),
+              Container(
+                width: 0.95.wp,
+                child: DateTimeField(
+                  format: format,
+                  decoration: InputDecoration(
+                    labelText: "To",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                  ),
+                  onShowPicker: (context, currentValue) async {
+                    final date = await showDatePicker(
+                      context: context,
+                      firstDate: DateTime.now(),
+                      initialDate: currentValue ?? DateTime.now(),
+                      lastDate: DateTime(2025),
+                    );
+                    if (date != null) {
+                      final time = await showTimePicker(
+                        context: context,
+                        initialTime: TimeOfDay.fromDateTime(
+                            currentValue ?? DateTime.now()),
+                      );
+                      return DateTimeField.combine(date, time);
+                    } else {
+                      return currentValue;
+                    }
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Title extends StatelessWidget {
+  final title;
+  final icon;
+
+  Title(this.title, this.icon);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Icon(icon),
+        SizedBox(width: 10),
+        Text(
+          title,
+          style: TextStyle(
+            fontSize: 18,
+          ),
+        ),
+      ],
     );
   }
 }
