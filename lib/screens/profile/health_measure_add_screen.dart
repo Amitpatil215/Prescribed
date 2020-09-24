4b9efcaf82a2324869d813c200ec48e7ea41bc79
Amit Patil <amitgpatil215@gmail.com>
2020-09-24 22:21:50 +0530

    Chaning raised button theme in main & adding floating button in health measures screen

--- lib/screens/profile/health_measure_add_screen.dart
+++ lib/screens/profile/health_measure_add_screen.dart
@@ -29,178 +29,174 @@ class _HealthMeasureAddScreenState extends State<HealthMeasureAddScreen> {
   Widget build(BuildContext context) {
     var _userData = Provider.of<Patient>(context, listen: true);
     return Scaffold(
-        resizeToAvoidBottomPadding: false,
-        backgroundColor: Theme.of(context).backgroundColor,
-        appBar: AppBar(
-          title: Text("Edit Profille"),
-        ),
-        body: Container(
-          margin: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              children: [
-                Container(
-                  margin: EdgeInsets.symmetric(vertical: 10),
-                  child: Row(
-                    children: [
-                      Icon(
-                        FlutterIcons.droplet_fea,
-                        size: 30,
-                      ),
-                      SizedBox(
-                        width: 15,
-                      ),
-                      Expanded(
-                        child: TextFormField(
-                          initialValue: _userData.bloodSugar != null
-                              ? _userData.bloodSugar.toString()
-                              : "",
-                          decoration: InputDecoration(
-                            hintText: "e.g 120",
-                            labelText: "Blood Sugar",
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(5),
-                            ),
-                          ),
-                          keyboardType: TextInputType.number,
-                          onSaved: (value) {
-                            if (value == null) {
-                              value = "";
-                            }
-                            _userData.bloodSugar = double.tryParse(value);
-                          },
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.symmetric(vertical: 10),
-                  child: Row(
-                    children: [
-                      Icon(
-                        FlutterIcons.cube_unfolded_mco,
-                        size: 30,
-                      ),
-                      SizedBox(
-                        width: 15,
-                      ),
-                      Expanded(
-                        child: TextFormField(
-                          initialValue: _userData.bloodPressure != null
-                              ? _userData.bloodPressure.toString()
-                              : "",
-                          decoration: InputDecoration(
-                            hintText: "e.g 120",
-                            labelText: "Blood Pressure",
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(5),
-                            ),
-                          ),
-                          onSaved: (value) {
-                            if (value == null) {
-                              value = "";
-                            }
-                            _userData.bloodPressure = double.tryParse(value);
-                          },
-                          keyboardType: TextInputType.number,
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.symmetric(vertical: 10),
-                  child: Row(
-                    children: [
-                      Icon(
-                        FlutterIcons.heartbeat_faw,
-                        size: 30,
-                      ),
-                      SizedBox(
-                        width: 15,
-                      ),
-                      Expanded(
-                        child: TextFormField(
-                          initialValue: _userData.heartRate != null
-                              ? _userData.heartRate.toString()
-                              : "",
-                          decoration: InputDecoration(
-                            hintText: "e.g. 98",
-                            labelText: "Heart Rate",
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(5),
-                            ),
-                          ),
-                          onSaved: (value) {
-                            if (value == null) {
-                              value = "";
-                            }
-                            _userData.heartRate = double.tryParse(value);
-                          },
-                          keyboardType: TextInputType.number,
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.symmetric(vertical: 10),
-                  child: Row(
-                    children: [
-                      Icon(
-                        FlutterIcons.description_mdi,
-                        size: 30,
-                      ),
-                      SizedBox(
-                        width: 15,
-                      ),
-                      Expanded(
-                        child: TextFormField(
-                          initialValue: _userData.allergy != null
-                              ? _userData.allergy
-                              : "",
-                          decoration: InputDecoration(
-                            hintText: "Dust allergy",
-                            labelText: "Allergy",
-                            alignLabelWithHint: true,
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(5),
-                            ),
-                          ),
-                          onSaved: (value) {
-                            if (value.isEmpty) value = null;
-                            _userData.allergy = value;
-                          },
-                          keyboardType: TextInputType.number,
-                          maxLines: 4,
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-                Spacer(),
-                Container(
-                  margin: EdgeInsets.only(bottom: 25),
-                  child: RaisedButton(
-                    child: Text(
-                      "Save",
-                      style: TextStyle(
-                        color: Colors.white,
-                      ),
+      resizeToAvoidBottomPadding: false,
+      backgroundColor: Theme.of(context).backgroundColor,
+      appBar: AppBar(
+        title: Text("Edit Profille"),
+      ),
+      body: Container(
+        margin: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 10),
+                child: Row(
+                  children: [
+                    Icon(
+                      FlutterIcons.droplet_fea,
+                      size: 30,
                     ),
-                    onPressed: () {
-                      _saveForm(context, _userData);
-                    },
-                    elevation: 5,
-                  ),
+                    SizedBox(
+                      width: 15,
+                    ),
+                    Expanded(
+                      child: TextFormField(
+                        initialValue: _userData.bloodSugar != null
+                            ? _userData.bloodSugar.toString()
+                            : "",
+                        decoration: InputDecoration(
+                          hintText: "e.g 120",
+                          labelText: "Blood Sugar",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                        ),
+                        keyboardType: TextInputType.number,
+                        onSaved: (value) {
+                          if (value == null) {
+                            value = "";
+                          }
+                          _userData.bloodSugar = double.tryParse(value);
+                        },
+                      ),
+                    )
+                  ],
                 ),
-              ],
-            ),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 10),
+                child: Row(
+                  children: [
+                    Icon(
+                      FlutterIcons.cube_unfolded_mco,
+                      size: 30,
+                    ),
+                    SizedBox(
+                      width: 15,
+                    ),
+                    Expanded(
+                      child: TextFormField(
+                        initialValue: _userData.bloodPressure != null
+                            ? _userData.bloodPressure.toString()
+                            : "",
+                        decoration: InputDecoration(
+                          hintText: "e.g 120",
+                          labelText: "Blood Pressure",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                        ),
+                        onSaved: (value) {
+                          if (value == null) {
+                            value = "";
+                          }
+                          _userData.bloodPressure = double.tryParse(value);
+                        },
+                        keyboardType: TextInputType.number,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 10),
+                child: Row(
+                  children: [
+                    Icon(
+                      FlutterIcons.heartbeat_faw,
+                      size: 30,
+                    ),
+                    SizedBox(
+                      width: 15,
+                    ),
+                    Expanded(
+                      child: TextFormField(
+                        initialValue: _userData.heartRate != null
+                            ? _userData.heartRate.toString()
+                            : "",
+                        decoration: InputDecoration(
+                          hintText: "e.g. 98",
+                          labelText: "Heart Rate",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                        ),
+                        onSaved: (value) {
+                          if (value == null) {
+                            value = "";
+                          }
+                          _userData.heartRate = double.tryParse(value);
+                        },
+                        keyboardType: TextInputType.number,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 10),
+                child: Row(
+                  children: [
+                    Icon(
+                      FlutterIcons.description_mdi,
+                      size: 30,
+                    ),
+                    SizedBox(
+                      width: 15,
+                    ),
+                    Expanded(
+                      child: TextFormField(
+                        initialValue:
+                            _userData.allergy != null ? _userData.allergy : "",
+                        decoration: InputDecoration(
+                          hintText: "Dust allergy",
+                          labelText: "Allergy",
+                          alignLabelWithHint: true,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                        ),
+                        onSaved: (value) {
+                          if (value.isEmpty) value = null;
+                          _userData.allergy = value;
+                        },
+                        keyboardType: TextInputType.number,
+                        maxLines: 4,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
           ),
-        )
-        //  ),
-        );
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton.extended(
+        label: Text(
+          "Save",
+          style: TextStyle(color: Colors.black),
+        ),
+        isExtended: true,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30),
+        ),
+        onPressed: () {
+          _saveForm(context, _userData);
+        },
+      ),
+    );
   }
 }
