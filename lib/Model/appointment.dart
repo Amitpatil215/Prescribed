6e3407302a5144ca4bda19c1338681ff5e2f0a6b
Amit Patil <amitgpatil215@gmail.com>
2020-10-27 11:23:50 +0530

    Doctor Model modified

--- lib/Model/appointment.dart
+++ lib/Model/appointment.dart
@@ -1,19 +1,14 @@
 import 'package:flutter/cupertino.dart';
 
-enum Mode {
-  TeleMedication,
-  PhysicalConsultation,
-}
-
 class Appointment {
-  final String id;
-  final Mode mode;
+  final String appointmentId;
+  final String mode;
   final DateTime time;
   final String doctorId;
   final String patientId;
 
   Appointment({
-    @required this.id,
+    @required this.appointmentId,
     @required this.mode,
     @required this.time,
     @required this.doctorId,
